4149d936e5f51729490ffccf09ca2ae14aae8d4f
andresdvx <andresdavidpachecocuadro@gmail.com>
2024-12-03 14:51:58 -0500

    numpy - array indexing

--- 04-numpy/array_indexing.py
+++ 04-numpy/array_indexing.py
@@ -2,4 +2,18 @@ import numpy as np
 
 arr = np.arange(0,11)
 
-print(arr[:4])
+print(arr)
+
+sliced_array = arr[0:5]
+
+print('sliced array:', sliced_array)
+
+copied_array = arr.copy()
+
+copied_array[:] = 7 # cambiar todos los valores de un array
+
+print('copied array:', copied_array)
+
+matriz = np.array([[5,10,15], [20,25,30], [35,40,45]], dtype=int)
+
+print(matriz)
